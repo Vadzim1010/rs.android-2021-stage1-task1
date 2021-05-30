da97ec83793170645846aeb28f3f0a5e5f4315b6
Vadzim <1010muad.dib@gmail.com>
2021-05-30 19:28:44 +0300

    feat: done tasks

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,44 @@
 package subtask1
 
+import java.util.*
+
 class HappyArray {
+
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+
+
+        val happyArray = arrayListOf<Int>()
+
+        for (value in sadArray) {
+            happyArray.add(value)
+        }
+
+        var isArrayHappy = true
+
+        var index: Int
+
+
+        while (isArrayHappy) {
+            isArrayHappy = false
+            index = 0
+            for (value in happyArray) {
+
+                if (index - 1 >= 0 && index + 1 < happyArray.size) {
+                    if (value > happyArray[index - 1] + happyArray[index + 1]) {
+                        happyArray[index] = 0
+                        isArrayHappy = true
+                        index -= 1
+                    }
+                }
+                index += 1
+            }
+            happyArray.removeIf{it == 0}
+        }
+        println(happyArray.toString())
+
+
+        return happyArray.toIntArray()
     }
 }
--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,67 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var arr: Array<String> = arrayOf()
+        var counter = 0
+        var firstLetterIndex = 0
+        var duplicateCount = 0
+        var char: CharSequence
+
+        for (y in firstLetterIndex until inputString.length) {
+            char = inputString.subSequence(y, y + 1)
+
+            if (inputString.subSequence(y, y + 1) == "<") {
+                firstLetterIndex = counter + 1
+                for (i in firstLetterIndex until inputString.length) {
+                    char = inputString.subSequence(i, i + 1)
+                    if (inputString.subSequence(i, i + 1) == "<") {
+                        duplicateCount += 1
+                    }
+                    if (inputString.subSequence(i, i + 1) == ">") {
+                        if (duplicateCount == 0) {
+                            arr += inputString.substring(firstLetterIndex, i)
+                            counter = y
+                            duplicateCount = 0
+                            break
+                        } else duplicateCount -= 1
+                    }
+                    counter += 1
+                }
+            } else if (inputString.subSequence(y, y + 1) == "[") {
+                firstLetterIndex = counter + 1
+                for (i in firstLetterIndex until inputString.length) {
+                    if (inputString.subSequence(i, i + 1) == "[") {
+                        duplicateCount += 1
+                    }
+                    if (inputString.subSequence(i, i + 1) == "]") {
+                        if (duplicateCount == 0) {
+                            arr += inputString.substring(firstLetterIndex, i)
+                            counter = y
+                            duplicateCount = 0
+                            break
+                        } else duplicateCount -= 1
+                    }
+                    counter += 1
+                }
+            } else if (inputString.subSequence(y, y + 1) == "(") {
+                firstLetterIndex = counter + 1
+                for (i in firstLetterIndex until inputString.length) {
+                    if (inputString.subSequence(i, i + 1) == "(") {
+                        duplicateCount += 1
+                    }
+                    if (inputString.subSequence(i, i + 1) == ")") {
+                        if (duplicateCount == 0) {
+                            arr += inputString.substring(firstLetterIndex, i)
+                            counter = y
+                            break
+                        } else duplicateCount -= 1
+                    }
+                    counter += 1
+                }
+            }
+            counter += 1
+        }
+        println(arr.contentToString())
+        return arr
     }
 }
